a8202299b2e55d69aad45695c7782f5672e21d9b
patelsweta2 <80040744+patelsweta2@users.noreply.github.com>
2023-11-29 19:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+	const imageContainer = document.getElementById('flex');
+	const resetButton = document.getElementById('reset');
+	const verifyButton = document.getElementById('verify');
+	const para = document.getElementById('para');
+	const h = document.getElementById('h');
+})
